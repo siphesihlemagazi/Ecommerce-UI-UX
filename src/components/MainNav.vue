4c045ca4a1b2f1156c3e677ca7eb31d70a2253d0
Siphesihle Magazi <siphesihlemagazi@outlook.com>
2022-07-26 09:37:13 -0700

    Initial commit

---
+++ src/components/MainNav.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand">Main Navbar</a>
+        <div class="d-flex">
+          <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+
+export default {
+  name: 'App',
+  components: {
+    FontAwesomeIcon,
+  }
+}
+</script>
+
